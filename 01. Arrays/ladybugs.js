1fa7aa4300876ba86da65830f508a75d9fd51b7b
Aleksandar Petkov <iskander9207@icloud.com>
2023-05-06 09:05:55 +0300

    Update ladybug.js
    working on solving ladybug.js task from Exercise: Array

--- 01. Arrays/ladybugs.js
+++ 01. Arrays/ladybugs.js
@@ -1,21 +1,45 @@
 function ladybugs(array) {
 
-    let fieldSize = array[0];
-    let positions = array[1].split(" ");
-    let finalResult = [];
-    for (let i = 2; i < array.length; i++) {
-        let command = array[i].split(" ");
-        let initial = command[0];
-        let direction = command[1];
-        let final = command[2];
-
-        
-
+    let fieldSize = array.shift();
+    let startIndexBugs = array.shift().split(' ').map((x) => Number(x));
+    let field = [];
+    let commands = array;
+    
+    for(let i = 0; i < fieldSize; i++) {
+        let currentBugPosition = startIndexBugs.shift();
+        if(i === currentBugPosition) {
+            field.push(1);
+        } else {
+            field.push(0);
+        }
     }
 
+    for(let command of commands) {
+        let [startIndex, direction, flyLength] = command.split(' ');
+        startIndex = +startIndex;
+        flyLength = +flyLength;
+        
+        if (field[startIndex] === 1) {
+            field[startIndex] = 0;
+            if (direction === 'right') {
+                while(field[startIndex + flyLength] != 0 && field[startIndex + flyLength] != undefined) {
+                    flyLength += flyLength;
+                }
+            } else {
+                while(field[startIndex - flyLength] != 0 && field[startIndex - flyLength] != undefined) {
+                    flyLength -= flyLength;
+                }
+            }
+            field[startIndex + flyLength] = 1;
+        }
+    }
+    let result = '';
+        for(let y = 0; y < fieldSize; y++) {
+        result += field[y] + ' ';
+    }
+
+    console.log(result)
     
 }
 
-ladybugs([ 3, '0 1',
-'0 right 1',
-'2 right 1' ]);
+ladybugs([ 5, '3', '3 left 2', '1 left -2']);
