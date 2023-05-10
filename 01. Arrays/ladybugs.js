1df48d7d5d30e942f626e2907bc4c91ae053f0fc
Aleksandar Petkov <iskander9207@icloud.com>
2023-05-10 09:20:39 +0300

    updated ladybugs.js

--- 01. Arrays/ladybugs.js
+++ 01. Arrays/ladybugs.js
@@ -5,41 +5,54 @@ function ladybugs(array) {
     let field = [];
     let commands = array;
     
+
     for(let i = 0; i < fieldSize; i++) {
-        let currentBugPosition = startIndexBugs.shift();
-        if(i === currentBugPosition) {
-            field.push(1);
-        } else {
+
+        for(let y = 0; y < startIndexBugs.length; y++) {
+            if(startIndexBugs[y] === i) {
+                field.push(1);
+                startIndexBugs.shift();
+            } else {
+                field.push(0);
+            }
+        }
+        if(startIndexBugs.length == 0) {
             field.push(0);
         }
+
     }
 
     for(let command of commands) {
         let [startIndex, direction, flyLength] = command.split(' ');
         startIndex = +startIndex;
         flyLength = +flyLength;
-        
+       
         if (field[startIndex] === 1) {
             field[startIndex] = 0;
             if (direction === 'right') {
                 while(field[startIndex + flyLength] != 0 && field[startIndex + flyLength] != undefined) {
                     flyLength += flyLength;
                 }
+                field[startIndex + flyLength] = 1;
             } else {
                 while(field[startIndex - flyLength] != 0 && field[startIndex - flyLength] != undefined) {
                     flyLength -= flyLength;
                 }
+                field[startIndex - flyLength] = 1;
             }
-            field[startIndex + flyLength] = 1;
         }
     }
+
     let result = '';
         for(let y = 0; y < fieldSize; y++) {
         result += field[y] + ' ';
     }
 
-    console.log(result)
-    
+    console.log(result);
 }
 
-ladybugs([ 5, '3', '3 left 2', '1 left -2']);
+ladybugs([ 3, '0 1',
+
+'0 right 1',
+
+'2 right 1' ]);
